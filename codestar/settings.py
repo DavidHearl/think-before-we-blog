aed7e7dd55606ce7bb5807b6988ce391f742aa51
DavidHearl <david.hearl@outlook.com>
2022-04-04 10:45:28 +0000

    changed wgsi application in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["think-before-we-blog.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['think-before-we-blog.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'codestar.wsgi.application'
+WSGI_APPLICATION = 'think-before-we-blog.wsgi.application'
 
 
 # Database
